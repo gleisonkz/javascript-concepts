46a7b331f26cb311c8f0e274f6c3f01d002c5cc1
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2021-05-07 16:53:49 -0300

    ✨ feat: reduceright example

---
+++ functions/array/reduceRight.js
@@ -0,0 +1,12 @@
+{
+  /* Array.Prototype.reduceRight
+
+reduzir uma coleção de itens a um único valor começando pelo final do array.*/
+
+  function reverse(string) {
+    return [...string].reduceRight((acc, cur) => (acc += cur), "");
+  }
+
+  const result = reverse("Está frase ficará ao contrário");
+  console.log(result);
+}
