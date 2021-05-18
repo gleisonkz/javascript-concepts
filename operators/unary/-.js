bd85a379965a25556428a17a1c13672d4832888e
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2021-05-17 23:58:37 -0300

    ✨ feat: unary negation operator example

---
+++ operators/unary/-.js
@@ -0,0 +1,11 @@
+const x = 1;
+const y = -3;
+const z = 5n;
+
+console.log(-x);
+console.log(-y);
+console.log(-"");
+console.log(-true);
+console.log(-false);
+console.log(-"hello");
+console.log(-z);
