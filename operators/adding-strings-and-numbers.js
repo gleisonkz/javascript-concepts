61d000bebf3d2d7209e4857157270cda6b933297
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2021-05-17 23:49:56 -0300

    ✨ feat: adding-strings-and-numbers example

---
+++ operators/adding-strings-and-numbers.js
@@ -0,0 +1,14 @@
+{
+  const x = 5,
+    y = "5";
+  const z = x + y;
+  console.log(z);
+}
+{
+  const x = 5 + 5;
+  const y = "5" + 5;
+  const z = "Hello" + 5;
+  console.log(x, `Type: ${typeof x}`);
+  console.log(y, `Type: ${typeof y}`);
+  console.log(z, `Type: ${typeof z}`);
+}
