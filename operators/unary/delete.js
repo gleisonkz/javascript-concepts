4e41ef6b0880c4029db16dbbaf5971c637ff136f
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2021-05-18 21:50:17 -0300

    ✨ feat: delete operator example

---
+++ operators/unary/delete.js
@@ -0,0 +1,39 @@
+// Unary delete operator
+// remove uma propriedade de um objeto
+// caso a mesma não seja uma Non-configurable property
+
+{
+  const employee = {
+    firstName: "John",
+    lastName: "Doe",
+  };
+
+  console.log(employee.firstName);
+  console.log(delete employee.firstName);
+
+  // Caso tente deletar uma propriedade que não existe
+  // também será retornado true
+  console.log(delete employee.UnknownProperty);
+  console.log(employee);
+}
+
+{
+  /* Quando uma propriedade é marcada como não configurável, 
+   a exclusão não terá nenhum efeito e retornará falso. 
+   No strict mode, isso gerará um TypeError. */
+
+  let car = {};
+  Object.defineProperty(car, "color", { configurable: false, value: "red" });
+  console.log(delete car.color);
+  console.log(car.color);
+}
+
+{
+  /* Quando você exclui um elemento de um array,
+    o length do array não é afetado. 
+   */
+  const trees = ["redwood", "bay"];
+  delete trees[1];
+  console.log(trees);
+  console.log(trees.length);
+}
