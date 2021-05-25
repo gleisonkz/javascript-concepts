b2bd420fe8e881aff50dbdaf2886daf60a1b1cd1
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2021-05-25 17:24:54 -0300

    ✨ feat: logical not operator example

---
+++ operators/unary/!.js
@@ -0,0 +1,11 @@
+console.log(!true);
+console.log(!false);
+console.log(!"");
+console.log(!"Cat");
+
+console.log(!!true);
+console.log(!!{});
+console.log(!!new Boolean(false));
+console.log(!!false);
+console.log(!!"");
+console.log(!!Boolean(false));
