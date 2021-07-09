93d2fb9a0e9a606762e86b2848e5c908840bd17a
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2021-07-09 16:56:45 -0300

    ✨ feat(array-prototype): of example

---
+++ methods/array/of.js
@@ -0,0 +1,13 @@
+{
+  /* Array.from
+  
+    método estático que cria uma instância de
+    um array preenchendo com 
+    os elementos passados nos parâmetros
+  */
+
+  console.log(Array.of("foo"));
+  console.log(Array.of(1, "a", false));
+  console.log(Array.of(null, undefined));
+  console.log(Array.of(""));
+}
