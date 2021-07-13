76d38e34868cb729ab40512888daefd13bab66c5
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2021-07-13 14:04:45 -0300

    ✨ feat(array-prototype): shift example

---
+++ methods/array/shift.js
@@ -0,0 +1,18 @@
+/* Array.prototype.shift
+
+remove e retorna o primeiro item de um array.
+*/
+
+const plants = ["broccoli", "cauliflower", "tomato"];
+const removedPlant = plants.shift();
+console.log(removedPlant);
+console.log(plants);
+
+console.log(plants.shift());
+console.log(plants);
+
+console.log(plants.shift());
+console.log(plants);
+
+console.log(plants.shift());
+console.log(plants);
