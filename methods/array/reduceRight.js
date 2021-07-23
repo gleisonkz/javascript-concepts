92e28628a37299a4be84716ddefb62de931cddff
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2021-07-23 06:46:24 -0300

    📦 refactor(array-prototype): update reduceRight example

--- methods/array/reduceRight.js
+++ methods/array/reduceRight.js
@@ -4,7 +4,7 @@
 reduzir uma coleção de itens a um único valor começando pelo final do array.*/
 
   function reverse(string) {
-    return [...string].reduceRight((acc, cur) => (acc += cur), "");
+    return [...string].reduceRight((acc, cur) => `${acc}${cur}`, "");
   }
 
   const result = reverse("Está frase ficará ao contrário");
