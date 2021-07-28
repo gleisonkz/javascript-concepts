1a78872b79795715b4eeea3873a022542094c493
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2021-07-28 18:09:13 -0300

    ✨ feat: initialization variables examples

---
+++ var_x_let_x_const/initialization.js
@@ -0,0 +1,11 @@
+// apenas var e let podem ser declaradas sem inicializar
+
+var user;
+console.log(user);
+
+let age;
+console.log(age);
+
+const height; 
+// Syntax Error pois toda const deve ser inicializada
+
