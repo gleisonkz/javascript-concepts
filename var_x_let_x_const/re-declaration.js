5ab889d83ec0b6d837aab7faa06f6b7dc4b9b793
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2021-07-28 18:09:30 -0300

    📦 refactor: update declaration example

--- var_x_let_x_const/re-declaration.js
+++ var_x_let_x_const/re-declaration.js
@@ -1,3 +1,14 @@
+// não é possível re-declarar variáveis usando let e const
+
+var user = "John";
+var user = "Doe";
+
+console.log(user); // ok pois var pode ser re-declarada
+
 const z = 1;
-const z = 3;
+const z = 3; // erro pois não é possível re-declarar usando const
 console.log(z);
+
+let y = 1;
+let y = 3; // erro pois não é possível re-declarar usando let
+console.log(y);
