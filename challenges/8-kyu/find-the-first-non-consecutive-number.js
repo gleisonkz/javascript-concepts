c41c59dc7a1c788015ebc36a513ac606f8b1da0f
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2021-08-16 23:50:48 -0300

    ✨ feat(kata): find-the-first-non-consecutive-number solution

--- challenges/8-kyu/find-the-first-non-consecutive-number.js
+++ challenges/8-kyu/find-the-first-non-consecutive-number.js
@@ -1,70 +1,68 @@
 /*
 
-Kata Nível 8 kyu - Find the first non-consecutive number
+Kata Nível 8 kyu - Find the smallest integer in the array
 
-Sua tarefa é encontrar o primeiro elemento de um array que não seja consecutivo.
-
-Por não consecutivo, não queremos dizer exatamente 1 maior do que o elemento anterior da matriz.
-
-Por exemplo. Se tivermos um array com [1,2,3,4,6,7,8], 
-
-então 1, 2, 3 e 4 são todos consecutivos, mas 6 não, 
-então esse é o primeiro número não consecutivo.
-
-Se toda o array for consecutivo, retorne null.
-
-Notas:
-
-- O array matriz sempre terá pelo menos 2 elementos,e todos os elementos serão números. 
-- Os números também serão únicos e em ordem crescente. 
-- Os números podem ser positivos ou negativos e o primeiro não consecutivo também pode ser!
-
-Você pode escrever uma solução que retornará null  para [] e [x] embora? 
-(Este é um array vazio e um com um único número e não foi testado, mas você pode escrever seu próprio teste de exemplo.)
-
+Implemente a função abaixo que deve retornar o menor inteiro contigo no array.
 */
 
-function firstNonConsecutive(arr) {
-  for (let index = 0; index < arr.length; index++) {
-    const currentNumber = arr[index];
-    const nextNumber = arr[index + 1];
+function findSmallestInt(numbers) {
+  return Math.min(...numbers);
+}
 
-    if (nextNumber === undefined) continue;
+function findSmallestInt(numbers) {
+  return numbers.reduce((a, b) => Math.min(a, b));
+}
 
-    const isNonConsecutive = currentNumber + 1 !== nextNumber;
-    if (isNonConsecutive) return nextNumber;
-  }
-  return null;
+function findSmallestInt(numbers) {
+  return Math.min.apply(null, numbers);
+}
+
+function findSmallestInt(numbers) {
+  return numbers.reduce((min, cur) => (min < cur ? min : cur));
+}
+function findSmallestInt(numbers) {
+  [min] = numbers.sort((a, b) => a - b);
+  return min;
 }
 
 {
-  // Teste 1 deve retornar 6
-  const sample = [1, 2, 3, 4, 6, 7, 8];
-  console.log(firstNonConsecutive(sample));
-  console.log(assert(sample, 6));
+  // Teste 1 deve retornar 8
+  const sample = [78, 56, 232, 12, 8];
+  const expected = 8;
+  console.log(findSmallestInt(sample));
+  console.log(assert(sample, expected));
 }
 
 {
-  // Teste 2 deve retornar 0
-  const sample = [-3, -2, 0, 1, 2];
-  console.log(firstNonConsecutive(sample));
-  console.log(assert(sample, 0));
+  // Teste 2 deve retornar 12
+  const sample = [78, 56, 232, 12, 18];
+  const expected = 12;
+  console.log(findSmallestInt(sample));
+  console.log(assert(sample, expected));
 }
-
 {
-  // Teste 3 deve retornar -1
-  const sample = [-3, -1, 0, 1, 2];
-  console.log(firstNonConsecutive(sample));
-  console.log(assert(sample, -1));
+  // Teste 3 deve retornar 56
+  const sample = [78, 56, 232, 412, 228];
+  const expected = 56;
+  console.log(findSmallestInt(sample));
+  console.log(assert(sample, expected));
 }
-
 {
-  // Teste 4 deve retornar null
-  const sample = [1, 2, 3, 4];
-  console.log(firstNonConsecutive(sample));
-  console.log(assert(sample, null));
+  // Teste 4 deve retornar 0
+  const sample = [78, 56, 232, 12, 0];
+  const expected = 0;
+  console.log(findSmallestInt(sample));
+  console.log(assert(sample, expected));
+}
+{
+  // Teste 5 deve retornar 1
+  const sample = [1, 56, 232, 12, 8];
+  const expected = 1;
+  console.log(findSmallestInt(sample));
+  console.log(assert(sample, expected));
 }
 
 function assert(sample, expected) {
-  return firstNonConsecutive(sample) === expected ? "OK" : "Algo deu errado";
+  const resultFn = findSmallestInt(sample);
+  return resultFn === expected ? "OK" : "Algo deu errado";
 }
