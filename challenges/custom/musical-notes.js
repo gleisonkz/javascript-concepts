f5797189b5bc6af3925a87f8c7895cbc0582ab74
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2021-09-14 19:26:24 -0300

    ✨ feat(kata): musical-notes

--- challenges/custom/musical-notes.js
+++ challenges/custom/musical-notes.js
@@ -1,5 +1,5 @@
 /*
-Kata Nível 8 kyu - getNote
+Kata Nível 8 kyu - Musical Notes
 
 Dado o seguinte array 
 const notes = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
@@ -12,52 +12,78 @@ a contagem deve ser feita em modo circular.
 
 function getNote(number) {
   const isNegative = number < 0;
+  const is2octaves = number > 12;
+
   const getSymbol = (index) => notes[index % notes.length];
+  const getPositiveIndex = (index) => (is2octaves ? index - 12 : index);
+
   let notes = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
-  let symbolIndex = isNegative ? Math.abs(number) - 1 : number;
+
+  let symbolIndex = isNegative ? Math.abs(number) - 1 : getPositiveIndex(number);
   if (isNegative) notes.reverse();
   return getSymbol(symbolIndex);
 }
 
 {
-  // Teste 1 deve retornar "C"
+  // Teste 1 para 0 deve retornar C
   const sample = 0;
   const expected = "C";
   console.log(getNote(sample));
   console.log(assert(sample, expected));
 }
+
 {
-  // Teste 2 deve retornar "B"
+  // Teste 2 para 11 deve retornar B
   const sample = 11;
   const expected = "B";
   console.log(getNote(sample));
   console.log(assert(sample, expected));
 }
+
 {
-  // Teste 3 deve retornar "F"
+  // Teste 3 para 5 deve retornar F
   const sample = 5;
   const expected = "F";
   console.log(getNote(sample));
   console.log(assert(sample, expected));
 }
+
 {
-  // Teste 4 deve retornar "D#"
+  // Teste 4 para 15 deve retornar D#
   const sample = 15;
   const expected = "D#";
   console.log(getNote(sample));
   console.log(assert(sample, expected));
 }
+
 {
-  // Teste 5 deve retornar "B"
+  // Teste 5 para -1 deve retornar B
   const sample = -1;
   const expected = "B";
   console.log(getNote(sample));
   console.log(assert(sample, expected));
 }
+
 {
-  // Teste 6 deve retornar "C"
+  // Teste 6 para -12 deve retornar C
   const sample = -12;
   const expected = "C";
+  console.log(getNote(sample));
+  console.log(assert(sample, expected));
+}
+
+{
+  // Teste 7 para -25 deve retornar "B"
+  const sample = -25;
+  const expected = "B";
+  console.log(getNote(sample));
+  console.log(assert(sample, expected));
+}
+
+{
+  // Teste 8 para 25 deve retornar "C#"
+  const sample = 25;
+  const expected = "C#";
   console.log(getNote(sample));
   console.log(assert(sample, expected));
 }
