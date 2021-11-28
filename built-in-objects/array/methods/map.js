5a9f9039a8cc7155ff003e8e59cd0fdc58e94d1a
Gleison Almeida <Gleison.RIbeiro-consultorexterno@rabobank.com>
2021-11-28 11:48:49 -0300

    📦 refactor(array): update map example

--- built-in-objects/array/methods/map.js
+++ built-in-objects/array/methods/map.js
@@ -1,10 +1,10 @@
 {
   /* Array.Prototype.map
-
-projetar um novo array a partir do array
-passado como parâmetro.*/
+  
+  projetar um novo array a partir do array
+  passado como parâmetro.*/
 
   const numbers = [1, 4, 9, 16];
-  numbersSquared = numbers.map((number) => Math.pow(number, 2));
-  console.log(numbersSquared);
+  const squaredNumbers = numbers.map((x) => x ** 2);
+  console.log(squaredNumbers);
 }
