bfa60416204f31f50556d043cfd2ea9ab57d59b8
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2022-02-15 18:33:41 -0300

    ✨ feat(array): every sample

---
+++ built-in-objects/array/methods/every.js
@@ -0,0 +1,22 @@
+{
+  /* Array.Prototype.every
+
+    retorna um booleano indicando se todos os elementos
+    do array satisfazem a condição passada como parâmetro.
+  */
+
+  {
+    // verificando se todas as letras são 'a'
+
+    const letters = ["a", "b", "c", "c", "c"];
+    let times = 0;
+
+    const hasA = letters.every((letter) => {
+      times++;
+      return letter === "a";
+    });
+
+    console.log(hasA);
+    console.log(times);
+  }
+}
